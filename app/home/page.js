bc19b214badff7c75b01ec95edd2c27df1019b8f
Your Name <haseebabid72@outlook.com>
2024-03-16 00:29:45 +0500

    new file:   app/Rectangle.png 	new file:   app/home/Rectangle.png 	new file:   app/home/home.css 	modified:   app/home/page.js 	modified:   app/page.js 	new file:   app/slides.json

--- app/home/page.js
+++ app/home/page.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
-const page = () => {
+import "./home.css"
+const page = ({data}) => {
   return (
-    <div className="text-black h-[70vh] flex items-center justify-center">
-      This is the HomePage of the Website
+    <div className="hero text-black h-[70vh] flex items-center justify-center">
+      {data.map((item, index) => {
+        return(
+          <>
+            <img src='./Rectangle.png' height={"100%"} width={"100"} alt={item.alt} key={index} />
+          </>
+        )
+      })}
+      {/* Will add the picture moving functionality when multiple pictures are provided */}
+
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import HomePage from "@/app/home/page"
+import {slides} from "./slides.json"
 export default function Home() {
   return (
     <div>
       {/* This will render the App at localhost:3000/ and localhost:3000/home both links will work.. I did this to higlight and tell the user at which page he is currently in..in order to edit the homepage, please go to the respective homepage component and edit it there.*/}
-      <HomePage/>
+      <HomePage data={slides}/>
     </div>
   );
 }
