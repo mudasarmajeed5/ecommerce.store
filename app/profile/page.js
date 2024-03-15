885116337e33870b80e760d86da115e465a27e43
mudasarmajeed5 <mudasarmajeed5@yahoo.com>
2024-03-15 17:38:59 +0500

    Added options to sign in through google and GITHUB

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,16 +1,15 @@
 "use client"
 import { useSession, signIn, signOut } from "next-auth/react"
-
 export default function Component() {
   const { data: session } = useSession()
   console.log(session);
   return (
-    <div className="flex justify-center items-center h-[60vh] text-center text-black font-bold">
+    <div className="flex justify-center items-center h-[60vh] text-center text-black">
       {session ? (
         <div>
           <div className="mt-4">
             Signed in as <span className="text-blue-500">{session.user.email}</span> <br />
-            Name: <span className="text-blue-500">{session.user.name}</span>
+            Welcome back <span className="text-blue-500">{session.user.name}</span>
           </div>
           <div className="mt-4 flex justify-center">
             <img src={session.user.image} alt="User" className="rounded-full w-20 h-20" />
@@ -26,10 +25,16 @@ export default function Component() {
         <div>
           <br />
           <button 
-            onClick={() => signIn("github")} 
+            onClick={() => signIn('github')} 
             className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
           >
             Sign in with Github
+          </button>
+          <button 
+            onClick={() => signIn('google')} 
+            className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+          >
+            Sign in with Google
           </button>
         </div>
       )}
