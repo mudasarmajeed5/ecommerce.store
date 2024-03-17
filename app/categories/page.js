717db7cdea7eac541f82ba01974fbcf6b9df6ed1
Your Name <haseebabid72@outlook.com>
2024-03-18 02:40:46 +0500

    modified:   app/categories/page.js

--- app/categories/page.js
+++ app/categories/page.js
@@ -1,16 +1,80 @@
 import React from 'react'
+const Categories = () => {
+    const CardsData = [
+        {
+            "title": "Foundation"
+        },
+        {
+            "title": "Concealer"
+        },
+        {
+            "title": "Powder"
+        },
+        {
+            "title": "Primer"
+        },
+        {
+            "title": "Blush"
+        },
+        {
+            "title": "Bronzer"
+        },
+        {
+            "title": "Highlighter"
+        },
+        {
+            "title": "Eyeshadow"
+        },
+        {
+            "title": "Eyeliner"
+        },
+        {
+            "title": "Mascara"
+        },
+        {
+            "title": "Lipstick"
+        },
+        {
+            "title": "Lip gloss"
+        },
+        {
+            "title": "Setting spray"
+        },
+        {
+            "title": "Makeup remover"
+        },
+        {
+            "title": "Setting powder"
+        },
+        {
+            "title": "Setting spray"
+        },
+        {
+            "title": "Makeup brushes/tools"
+        },
+        {
+            "title": "Contour products"
+        }
+    ];
 
-const page = () => {
-  return (
-    <div className='h-[70vh] flex flex-col justify-center items-center text-black font-bold'>
-      <h2>Categories Page</h2>
-      <ul className='flex text-xl mx-auto justify-between gap-4 list-none'>
-        <li>Games</li>
-        <li>Family pack</li>
-        <li>Kids Meal</li>
-      </ul>
-    </div>
-  )
-}
+    return (
+      <div className="w-full flex flex-wrap justify-center">
+        {CardsData.map((item) => {
+          // let unique_id = v4();
+          return (
+            <div key={item.title} className='card w-[250px] flex flex-col justify-between relative rounded-lg shadow-red-400 shadow-xl p-2 text-[--body-color] mx-4 my-4'>
+              <div className="image relative py-2">
+                <img className='w-full h-[200px] object-cover text-center rounded-md' src={item.image} alt="" />
+              </div>
+              <div className="item-desc flex justify-between flex-col pl-2">
+                <div className='text-lg font-mono'>{item.title}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+  
 
-export default page
+export default Categories
