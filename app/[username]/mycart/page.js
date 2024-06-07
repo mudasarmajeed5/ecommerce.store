4b6824aa5e9d327ca64b7718cc8667dc4fe3a029
mudasarmajeed5 <143939048+mudasarmajeed5@users.noreply.github.com>
2024-06-07 22:29:26 +0500

    Minor color improvements on mycart page

--- app/[username]/mycart/page.js
+++ app/[username]/mycart/page.js
@@ -105,19 +105,19 @@ const Mycart = () => {
         </div>
       </div>
     )}
-    <div className="flex justify-between md:flex-row flex-col">
+    <div className="flex justify-between md:flex-row flex-col min-h-screen">
       <div className="left-side-cart w-full">
         {cartitems.length > 0 && (<>
           <div className="text-center text-black text-2xl font-bold my-5">Your items</div>
           <div className="flex text-black max-h-[70vh] mt-5 overflow-auto w-full flex-col p-2">
             {cartitems.map((item, index) => (
               <div key={index} className="w-full mx-auto md:w-4/5 mb-3 ">
-                <div className="flex shadow-lg bg-[#d88984] rounded-lg overflow-hidden items-around relative">
+                <div className="flex border-black border rounded-lg overflow-hidden items-around relative">
                   <img src={item.image} alt={item.title} className="w-[100px] md:w-[150px] h-[100px] md:h-[150px] object-cover object-center" />
                   <div className="px-2 md:p-4">
                     <p className="text-md md:text-lg pt-1 font-semibold mb-2">{item.title}</p>
                     <p className="text-sm text-gray-900 my-2">Price: Rs.{item.price}</p>
-                    <button className='hover:bg-black bg-gray-900 md:text-md text-sm absolute bottom-2 text-white px-2 py-1 ' onClick={() => handleRemoveItem(item)}>Remove Item</button>
+                    <button className='hover:bg-[--body-color] bg-[--secondary-color] md:text-md text-sm absolute bottom-2 text-white rounded-full px-2 py-1 ' onClick={() => handleRemoveItem(item)}>Remove Item</button>
                   </div>
                 </div>
               </div>
@@ -126,14 +126,14 @@ const Mycart = () => {
         )}
       </div>
       {cartitems.length > 0 &&
-        <div className='right-side-cart w-full my-5 text-black'>
+        <div className='right-side-cart w-4/5 mx-auto my-5 text-black'>
           <div className="mb-3 mt-1 font-bold text-xl md:text-2xl text-center">Your Summary</div>
-          <div className="total-summary rounded-md p-4 overflow-hidden text-lg w-11/12 md:w-4/6 h-[50vh] bg-[--cart-theme] mx-auto flex flex-col item-end">
+          <div className="total-summary rounded-md p-4 overflow-hidden text-lg w-11/12 border border-black md:w-4/6 h-[50vh] mx-auto flex flex-col item-end">
             <div className="total-items font-bold h-fit flex gap-2 mt-3">
               <span>Total Items: </span><span>{cartitems.length}</span>
             </div>
-            <div className="h-3/4 overflow-auto shadow py-2 px-1">{cartitems.map((item, index) => (
-              <div key={index} className="w-full flex gap-1 flex-col px-2 py-2 mb-1 rounded-sm shadow-lg">
+            <div className="h-3/4 overflow-auto py-2 px-1">{cartitems.map((item, index) => (
+              <div key={index} className="w-full flex gap-1 flex-col px-2 py-2 mb-1 rounded-sm">
                 <p className="text-sm md:text-md flex w-full justify-between pt-1 font-semibold "><span>{item.title} </span> <span>Item Quantity x 1</span></p>
                 <p className="text-sm text-gray-900 my-2">Price: Rs.{item.price}</p>
               </div>
